330841c8adb09f96742693c140d2d4652b8fe6c2
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-01-03 16:07:51 +0900

    metadata/docs

--- src/lpdb/__init__.py
+++ src/lpdb/__init__.py
@@ -1,2 +1,34 @@
+"""
+Python interface for Liquipedia Database (LPDB) API
+"""
+
+import importlib.metadata as _metadata
+
 from .defs import *
 from .session import *
+
+__all__ = [
+    "OpponentType",
+    "Broadcasters",
+    "Company",
+    "Datapoint",
+    "ExternalMediaLink",
+    "LpdbError",
+    "LpdbWarning",
+    "LpdbSession",
+    "Match",
+    "MatchGame",
+    "MatchOpponent",
+    "Placement",
+    "Player",
+    "Series",
+    "SquadPlayer",
+    "StandingsEntry",
+    "StandingsTable",
+    "Team",
+    "Tournament",
+    "Transfer",
+    "TeamTemplate",
+]
+
+__version__ = _metadata.version("LPDB_python")
